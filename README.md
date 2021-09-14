c753e23fed4a30256a253f645a9483627113159e
Srajan Seth <srajanseth84@gmail.com>
2021-09-15 05:12:01 +0530

    New safe repository

---
+++ README.md
@@ -0,0 +1,49 @@
+# Food Vision 🍔as
+
+st.title("What's Food Vision ?")
+st.write("""
+FoodVision is an end-to-end **CNN Image Classification Model** which identifies the food in your image. 
+
+It can identify over 100 different food classes
+
+It is based upom a pre-trained Image Classification Model that comes with Keras and then retrained on the infamous **Food101 Dataset**.
+
+**Accuracy :** **`85%`**
+
+**Model :** **`EfficientNetB1`**
+
+**Dataset :** **`Food101`**
+""")
+
+
+
+
+
+
+
+
+
+
+## Requirements
+-----
+### Data File
+* [Food-101](http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz)
+(4.67GB)
+
+### Dependencies
+
+* [Tensorflow](https://github.com/tensorflow/tensorflow)
+* [Tensorflow-Hub](https://github.com/tensorflow/hub)
+* [Numpy](https://github.com/numpy/numpy)
+* [Matplotlib](https://github.com/matplotlib/matplotlib)
+* [Model-1-Inception-Resnet-V2](https://www.tensorflow.org/api_docs/python/tf/keras/applications/inception_resnet_v2/InceptionResNetV2)
+* [Model-2-EfficientNet]
+
+### Trained Model(To Deploy directly)
+
+* [Download Link](https://drive.google.com/file/d/1oYT2Kcy8mNOyCOyJq5b6maZ9g84WcCf1/view?usp=sharing) (400+ MB)
+
+
+## Authors
+
+- [@srajanseth84](https://github.com/srajanseth84)
---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import requests
+import time
+import os
+
+API_TOKEN = "Add Your token"
+st.set_page_config(page_title="Know the sentiment",
+                   page_icon="🧐")
+
+st.title("Know the Sentiment🧐")
+st.write("To know more about this app, visit [**GitHub**](https://github.com/srajanseth84/Sentiment-Classification)")
+
+
+API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+headers = {"Authorization":  API_TOKEN}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+st.write("### Enter Sentence")
+input = st.text_input(" ")
+button = st.button("🧐")
+
+if button and not input:
+    st.warning("⚠️ Please INPUT a Sentence ⚠️")
+
+try:
+    with st.spinner("Classifying Sentiment"):
+        if button and input:
+            output = query({"inputs": input})
+            for sentence in output:
+                for pred in sentence:
+                    col1 , col2 = st.columns(2)
+                    with col1:
+                        label_score = pred.get("label") +"      " + format(pred.get("score")*100,".2f") + "%"
+                        symbol = ""
+                        if pred["label"] == "NEGATIVE":
+                            symbol = "    👎"
+                        else:
+                            symbol = "    👍"
+                        if pred["score"] > 0.5:
+                            st.success(label_score + symbol)
+                        else:
+                            st.write(label_score)
+                    with col2:
+                        my_bar = st.progress(0)
+                        for percent_complete in range(int(pred["score"]*100)):
+                            time.sleep(0.01)
+                            my_bar.progress(percent_complete + 1)
+                            
+except:
+    st.warning("Some **Unexpected** Error happen")
+    st.warning("Please create a **Issue** on [Github](https://github.com/srajanseth84/Sentiment-Classification)")
+
+
+st.markdown("Created by **Srajan Seth**")
+st.markdown(body="""
+<th style="border:None"><a href="https://www.linkedin.com/in/srajan-seth-8713b3183/" target="blank"><img align="center" src="https://bit.ly/3wCl82U" alt="srajan-seth-8713b3183" height="40" width="40" /></a></th>
+<th style="border:None"><a href="https://github.com/srajanseth84" target="blank"><img align="center" src="https://bit.ly/3c2onZS" alt="srajanseth84" height="40" width="40" /></a></th>
+""", unsafe_allow_html=True)
